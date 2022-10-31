dc96c2f0980cc923613a1d0e42db90fa3c98ee68
CSK <cr2007@hw.ac.uk>
2022-10-31 09:50:11 +0400

    Minor commenting fixes

--- monster.pl
+++ monster.pl
@@ -161,7 +161,7 @@ moreEffectiveTypeMove(T, MV1, MV2) :-
 /* Move MV1 is more effective against MO2 than MV2 against MO1 */
 moreEffectiveMonsterMove(MO1, MO2, MV1, MV2) :- 
     /* Checks if the Monster has the move specified
-    *  else it doesn't continue with the sequence */
+       else it doesn't continue with the sequence */
     monsterMove(MO1, MV1),!,          % Move MV1 is a move of monster MO1
     monsterMove(MO2, MV2),!,          % Move MV1 is a move of monster MO1
 
